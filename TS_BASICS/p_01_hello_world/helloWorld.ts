8106622643b666d0878dafda22933d1fede371a7
Santiago <santiago.garcia1999@hotmail.com>
2021-01-10 18:29:43 -0500

    Created hello world in TypeScript for MHL 2021

---
+++ TS_BASICS/p_01_hello_world/helloWorld.ts
@@ -0,0 +1,11 @@
+// MY FIRST TYPESCRIPT PROJECT
+// Local Hack Day January 10th 2021
+// Santiago Garcia Arango
+
+let helloWorld = "Hello World, I am Santiago Garcia";
+//  ^ = let helloWorld: string
+
+const user = {
+    name: "Hayes",
+    id: 0,
+};
