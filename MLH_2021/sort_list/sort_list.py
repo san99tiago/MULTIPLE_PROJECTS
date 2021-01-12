ca38262569b6d91514e7c5a9ac35561332a172b3
Santiago <santiago.garcia1999@hotmail.com>
2021-01-12 13:28:18 -0500

    Created sort_list first approach with numerically bubble sort

---
+++ MLH_2021/sort_list/sort_list.py
@@ -0,0 +1,39 @@
+# LIST SORTING USING BUBBLE ALGORITHM
+# Santiago Garcia Arango
+# MLH2021
+
+class ListSorter:
+    def __init__(self, vector, sort_type="numerically", sort_algorithm="bubble"):
+        self.vector = vector
+        self.sort_type = sort_type
+
+        if sort_algorithm == "bubble":
+            self.bubble_sort()
+
+    def bubble_sort(self):
+        if self.sort_type == "numerically":
+            # Go through the vector as many times as there exists elements
+            for i in range(len(self.vector)):
+                # Only get to the previous of the last element of list...
+                # ... so that we don't run out of index
+                for j in range(len(self.vector) - 1):
+                    if self.vector[j] > self.vector[j + 1]:
+                        # Swap pair of elements when the needs to be sorting
+                        temp = self.vector[j]
+                        self.vector[j] = self.vector[j + 1]
+                        self.vector[j + 1] = temp
+            return
+
+
+if __name__ == "__main__":
+    # ---------------- TEST 1 ---------------
+    vector = [9, 3, 4, 2, 1, 5, 6, 7, 8]
+    print("ORIGINAL: ", vector)
+    LS = ListSorter(vector)
+    print("SORTED: ", LS.vector)
+
+
+
+
+
+
